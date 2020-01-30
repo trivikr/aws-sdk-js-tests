528e153d971b3b792b04fff0a0d3f3d300f93479
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-30 11:03:46 -0800

    chore: code for testing ElastiCache

--- src/node.js
+++ src/node.js
@@ -1,17 +1,35 @@
 const AWS = require("aws-sdk");
-const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { ElastiCache } = require("@aws-sdk/client-elasticache");
 const { REGION } = require("./config");
 
 (async () => {
   let response;
+  const name =
+    "TestCacheParameterGroup-" +
+    Math.random()
+      .toString(36)
+      .substring(2);
 
-  const v2Client = new AWS.DynamoDB({ region: REGION });
-  response = await v2Client.listTables().promise();
+  const params = {
+    Description: "TestCacheParameterGroup",
+    CacheParameterGroupFamily: "memcached1.4"
+  };
+
+  const v2Client = new AWS.ElastiCache({ region: REGION });
+  response = await v2Client
+    .createCacheParameterGroup({
+      ...params,
+      CacheParameterGroupName: name
+    })
+    .promise();
   console.log("Data returned by v2:");
   console.log(response);
 
-  const v3Client = new DynamoDB({ region: REGION });
-  response = await v3Client.listTables({});
+  const v3Client = new ElastiCache({ region: REGION });
+  response = await v3Client.createCacheParameterGroup({
+    ...params,
+    CacheParameterGroupName: `${name}-V3`
+  });
   console.log("\nData returned by v3:");
   console.log(response);
 })();
