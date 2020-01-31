58163bc0a3151726476e5f968707e26651cafb76
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-31 13:27:17 -0800

    feat: cloudfront listDistributions test

--- src/node.js
+++ src/node.js
@@ -1,17 +1,17 @@
 const AWS = require("aws-sdk");
-const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { CloudFront } = require("@aws-sdk/client-cloudfront");
 const { REGION } = require("./config");
 
 (async () => {
   let response;
 
-  const v2Client = new AWS.DynamoDB({ region: REGION });
-  response = await v2Client.listTables().promise();
+  const v2Client = new AWS.CloudFront({ region: REGION });
+  response = await v2Client.listDistributions().promise();
   console.log("Data returned by v2:");
-  console.log(response);
+  console.log(JSON.stringify(response, null, 2));
 
-  const v3Client = new DynamoDB({ region: REGION });
-  response = await v3Client.listTables({});
+  const v3Client = new CloudFront({ region: REGION });
+  response = await v3Client.listDistributions({});
   console.log("\nData returned by v3:");
-  console.log(response);
+  console.log(JSON.stringify(response, null, 2));
 })();
