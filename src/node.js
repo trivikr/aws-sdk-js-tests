d2adae9dbbff3edafef2f9db8d4cb24b4305cec6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-03 14:49:04 -0800

    wip: testing s3.listObjects

--- src/node.js
+++ src/node.js
@@ -1,9 +1,10 @@
-const AWS = require("aws-sdk");
 const { S3 } = require("@aws-sdk/client-s3");
 const { REGION } = require("./config");
 
+const { promisify } = require("util");
+const sleep = promisify(setTimeout);
+
 (async () => {
-  let response;
   const bucketName =
     "test-bucket-" +
     Math.random()
@@ -14,38 +15,32 @@ const { REGION } = require("./config");
   };
   const objectName = "ExampleObject.jpg";
 
-  const v2Client = new AWS.S3({ region: REGION });
+  const v3Client = new S3({ region: REGION });
+
   console.log(`Creating bucket ${bucketName}`);
-  await v2Client.createBucket(params).promise();
+  await v3Client.createBucket(params);
+
   console.log(`Waiting for "${bucketName}" bucket creation`);
-  await v2Client.waitFor("bucketExists", params).promise();
+  // waiter not available yet, just sleep for 5 seconds
+  await sleep(5000);
 
   console.log(`Putting object ${objectName} in ${bucketName}...`);
-  await v2Client
-    .putObject({
-      Body: "000000",
-      Bucket: bucketName,
-      Key: objectName
-    })
-    .promise();
+  await v3Client.putObject({
+    Body: "000000",
+    Bucket: bucketName,
+    Key: objectName
+  });
 
-  response = await v2Client.listObjects(params).promise();
-  console.log("Data returned by v2:");
-  console.log(JSON.stringify(response, null, 2));
-
-  const v3Client = new S3({ region: REGION });
-  response = await v3Client.listObjects(params);
+  const response = await v3Client.listObjects(params);
   console.log("\nData returned by v3:");
   console.log(JSON.stringify(response, null, 2));
 
-  console.log(`Deleting ${objectName} from ${bucketName}`);
-  await v2Client
-    .deleteObject({
-      Bucket: bucketName,
-      Key: objectName
-    })
-    .promise();
+  console.log(`\nDeleting ${objectName} from ${bucketName}`);
+  await v3Client.deleteObject({
+    Bucket: bucketName,
+    Key: objectName
+  });
 
   console.log(`Deleting bucket ${bucketName}`);
-  await v2Client.deleteBucket(params).promise();
+  await v3Client.deleteBucket(params);
 })();
