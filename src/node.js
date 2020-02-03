c85cb2c3bcbef324a440ec026c85782a7c3ee0c6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-03 15:34:34 -0800

    chore: code to empty bucket before testing

--- src/node.js
+++ src/node.js
@@ -1,15 +1,33 @@
 const { S3 } = require("@aws-sdk/client-s3");
 const { REGION } = require("./config");
 
-const { promisify } = require("util");
-const sleep = promisify(setTimeout);
-
 (async () => {
+  let response;
   // Bucket created from console for testing
   const bucketName = "test-bucket-fast-xml-parser";
-  const objectName = "ExampleObject.jpg";
+  const objectName =
+    "test-object-" +
+    Math.random()
+      .toString(36)
+      .substring(2);
 
   const v3Client = new S3({ region: REGION });
+
+  response = await v3Client.listObjects({ Bucket: bucketName });
+  if (Array.isArray(response.Contents)) {
+    console.log(`Emptying bucket ${bucketName}`);
+    for (const obj of response.Contents) {
+      console.log(`\nDeleting ${obj.Key} from ${bucketName}`);
+      await v3Client.deleteObject({
+        Bucket: bucketName,
+        Key: obj.Key
+      });
+    }
+  }
+
+  response = await v3Client.listObjects({ Bucket: bucketName });
+  console.log("\nResponse for empty bucket:");
+  console.log(JSON.stringify(response, null, 2));
 
   console.log(`Putting object ${objectName} in ${bucketName}...`);
   await v3Client.putObject({
@@ -17,10 +35,6 @@ const sleep = promisify(setTimeout);
     Bucket: bucketName,
     Key: objectName
   });
-
-  const response = await v3Client.listObjects({ Bucket: bucketName });
-  console.log("\nData returned by v3:");
-  console.log(JSON.stringify(response, null, 2));
 
   console.log(`\nDeleting ${objectName} from ${bucketName}`);
   await v3Client.deleteObject({
