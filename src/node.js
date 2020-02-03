f1e73a5c2f5b2ff395266593e25c945a046913ec
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-03 15:38:45 -0800

    chore: testing listObjects with 0,1,2 objects

--- src/node.js
+++ src/node.js
@@ -5,11 +5,7 @@ const { REGION } = require("./config");
   let response;
   // Bucket created from console for testing
   const bucketName = "test-bucket-fast-xml-parser";
-  const objectName =
-    "test-object-" +
-    Math.random()
-      .toString(36)
-      .substring(2);
+  const objectName = "test-object";
 
   const v3Client = new S3({ region: REGION });
 
@@ -36,9 +32,30 @@ const { REGION } = require("./config");
     Key: objectName
   });
 
+  response = await v3Client.listObjects({ Bucket: bucketName });
+  console.log("\nResponse for bucket with one object:");
+  console.log(JSON.stringify(response, null, 2));
+
+  const objectName2 = `${objectName}-2`;
+  console.log(`Putting object ${objectName2} in ${bucketName}...`);
+  await v3Client.putObject({
+    Body: "000000",
+    Bucket: bucketName,
+    Key: objectName2
+  });
+
+  response = await v3Client.listObjects({ Bucket: bucketName });
+  console.log("\nResponse for bucket with two objects:");
+  console.log(JSON.stringify(response, null, 2));
+
   console.log(`\nDeleting ${objectName} from ${bucketName}`);
   await v3Client.deleteObject({
     Bucket: bucketName,
     Key: objectName
+  });
+  console.log(`\nDeleting ${objectName2} from ${bucketName}`);
+  await v3Client.deleteObject({
+    Bucket: bucketName,
+    Key: objectName2
   });
 })();
