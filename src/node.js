1a0fff929b85104e9d124e108145508fe2a5ee72
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-04 15:22:50 -0800

    chore: sample code glacier createVault
    Blocked on https://github.com/aws/aws-sdk-js-v3/issues/849

--- src/node.js
+++ src/node.js
@@ -1,17 +1,29 @@
 const AWS = require("aws-sdk");
-const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+// const { Glacier } = require("@aws-sdk/client-glacier");
 const { REGION } = require("./config");
 
 (async () => {
   let response;
 
-  const v2Client = new AWS.DynamoDB({ region: REGION });
-  response = await v2Client.listTables().promise();
-  console.log("Data returned by v2:");
-  console.log(JSON.stringify(response, null, 2));
+  const v2Client = new AWS.Glacier({ region: REGION });
+  //const v3Client = new Glacier({ region: REGION });
 
-  const v3Client = new DynamoDB({ region: REGION });
-  response = await v3Client.listTables({});
-  console.log("\nData returned by v3:");
-  console.log(JSON.stringify(response, null, 2));
+  const vaultName = "TestVault";
+
+  const body = Buffer.alloc(0.25 * 1024 * 1024);
+  body.fill("0");
+
+  await v2Client.createVault({ vaultName: `${vaultName}V2` }).promise();
+  await v2Client
+    .waitFor("vaultExists", { vaultName: `${vaultName}V2` })
+    .promise();
+
+  response = await v2Client
+    .uploadArchive({
+      vaultName: `${vaultName}V2`,
+      body
+    })
+    .promise();
+  console.log("\nData returned by v2:");
+  console.log(response);
 })();
