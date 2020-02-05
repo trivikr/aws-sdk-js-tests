a72885af43459ff45c53ef51c7b7ffeb1c2eccb5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-04 16:14:07 -0800

    chore: pass valid checksum

--- src/node.js
+++ src/node.js
@@ -6,24 +6,32 @@ const { REGION } = require("./config");
   let response;
 
   const v2Client = new AWS.Glacier({ region: REGION });
-  //const v3Client = new Glacier({ region: REGION });
+  // const v3Client = new Glacier({ region: REGION });
 
   const vaultName = "TestVault";
 
   const body = Buffer.alloc(0.25 * 1024 * 1024);
   body.fill("0");
+  const { treeHash: checksum } = v2Client.computeChecksums(body);
 
-  await v2Client.createVault({ vaultName: `${vaultName}V2` }).promise();
-  await v2Client
-    .waitFor("vaultExists", { vaultName: `${vaultName}V2` })
-    .promise();
+  await v2Client.createVault({ vaultName }).promise();
+  await v2Client.waitFor("vaultExists", { vaultName }).promise();
 
   response = await v2Client
     .uploadArchive({
-      vaultName: `${vaultName}V2`,
-      body
+      vaultName,
+      body,
+      checksum
     })
     .promise();
   console.log("\nData returned by v2:");
   console.log(response);
+
+  const { archiveId } = response;
+  await v2Client
+    .deleteArchive({
+      vaultName,
+      archiveId
+    })
+    .promise();
 })();
