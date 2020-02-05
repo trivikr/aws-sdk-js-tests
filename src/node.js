285b94bf24f6475bb7dd5a78aafc5c7be9bb38f1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-04 16:27:35 -0800

    chore: test errors with incorrect checksum

--- src/node.js
+++ src/node.js
@@ -1,37 +1,41 @@
 const AWS = require("aws-sdk");
-// const { Glacier } = require("@aws-sdk/client-glacier");
+const { Glacier } = require("@aws-sdk/client-glacier");
 const { REGION } = require("./config");
 
 (async () => {
-  let response;
-
   const v2Client = new AWS.Glacier({ region: REGION });
-  // const v3Client = new Glacier({ region: REGION });
+  const v3Client = new Glacier({ region: REGION });
 
   const vaultName = "TestVault";
 
   const body = Buffer.alloc(0.25 * 1024 * 1024);
   body.fill("0");
-  const { treeHash: checksum } = v2Client.computeChecksums(body);
+  const incorrectChecksum = "00000000000000000000000000000000";
 
   await v2Client.createVault({ vaultName }).promise();
   await v2Client.waitFor("vaultExists", { vaultName }).promise();
 
-  response = await v2Client
-    .uploadArchive({
+  try {
+    await v2Client
+      .uploadArchive({
+        vaultName,
+        body,
+        checksum: incorrectChecksum
+      })
+      .promise();
+  } catch (error) {
+    console.log(`Error from v2:`);
+    console.log(JSON.stringify(error, null, 2));
+  }
+
+  try {
+    await v3Client.uploadArchive({
       vaultName,
       body,
-      checksum
-    })
-    .promise();
-  console.log("\nData returned by v2:");
-  console.log(response);
-
-  const { archiveId } = response;
-  await v2Client
-    .deleteArchive({
-      vaultName,
-      archiveId
-    })
-    .promise();
+      checksum: incorrectChecksum
+    });
+  } catch (error) {
+    console.log(`\nError from v3:`);
+    console.log(JSON.stringify(error, null, 2));
+  }
 })();
