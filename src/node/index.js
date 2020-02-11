ce9ebbfadce04e3aad3cc1f14e3070c45930683d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-10 21:07:51 -0800

    chore: test s3.selectObjectContent

--- src/node/index.js
+++ src/node/index.js
@@ -6,9 +6,17 @@ const { getV2Response, getV3Response } = require("../shared/utils");
 
   response = await getV2Response({ region: REGION });
   console.log("Data returned by v2:");
-  console.log(JSON.stringify(response, null, 2));
+  for await (const event of response.Payload) {
+    if (event.Records) {
+      console.log(event.Records.Payload.toString());
+    }
+  }
 
   response = await getV3Response({ region: REGION });
   console.log("\nData returned by v3:");
-  console.log(JSON.stringify(response, null, 2));
+  for await (const event of response.Payload) {
+    if (event.Records) {
+      console.log(event.Records.Payload.toString());
+    }
+  }
 })();
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -4,18 +4,36 @@ const {
   fromCognitoIdentityPool
 } = require("@aws-sdk/credential-provider-cognito-identity");
 const { CognitoIdentityClient } = require("@aws-sdk/client-cognito-identity");
-const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { S3 } = require("@aws-sdk/client-s3");
 
 const { REGION, IDENTITY_POOL_ID } = require("./config");
 
+const params = {
+  Bucket: "test-aws-sdk-js-v3-315",
+  Key: "airport-codes.csv",
+  Expression: "select * from s3object s where s.iso_country='AD'",
+  ExpressionType: "SQL",
+  RequestProgress: {
+    Enabled: true
+  },
+  InputSerialization: {
+    CSV: {
+      FileHeaderInfo: "Use"
+    }
+  },
+  OutputSerialization: {
+    CSV: {}
+  }
+};
+
 const getV2Response = async clientParams => {
-  const client = new AWS.DynamoDB(clientParams);
-  return client.listTables().promise();
+  const client = new AWS.S3(clientParams);
+  return client.selectObjectContent(params).promise();
 };
 
 const getV3Response = async clientParams => {
-  const client = new DynamoDB(clientParams);
-  return client.listTables({});
+  const client = new S3(clientParams);
+  return client.selectObjectContent(params);
 };
 
 const getV2BrowserResponse = async () => {
