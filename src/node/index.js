780e143ebc4754f119311c225adbe923c83f7216
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-03-27 10:40:25 -0700

    chore: update utils file to test kinesis in Node.js

--- src/node/index.js
+++ src/node/index.js
@@ -1,14 +1,22 @@
-const { REGION } = require("../shared/config");
-const { getV2Response, getV3Response } = require("../shared/utils");
+const {
+  beforeAll,
+  afterAll,
+  getV2Response,
+  getV3Response
+} = require("../shared/utils");
 
 (async () => {
   let response;
 
-  response = await getV2Response({ region: REGION });
+  await beforeAll();
+
+  response = await getV2Response();
   console.log("Data returned by v2:");
   console.log(JSON.stringify(response, null, 2));
 
-  response = await getV3Response({ region: REGION });
+  response = await getV3Response();
   console.log("\nData returned by v3:");
   console.log(JSON.stringify(response, null, 2));
+
+  await afterAll();
 })();
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -4,18 +4,48 @@ const {
   fromCognitoIdentityPool
 } = require("@aws-sdk/credential-provider-cognito-identity");
 const { CognitoIdentityClient } = require("@aws-sdk/client-cognito-identity");
-const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { Kinesis } = require("@aws-sdk/client-kinesis");
 
 const { REGION, IDENTITY_POOL_ID } = require("./config");
 
+let ShardIterator;
+
+const v2Client = new AWS.Kinesis({ region: REGION });
+const v3Client = new Kinesis({ region: REGION });
+const StreamName = `test-stream-${Math.floor(Math.random() * 10 ** 10)}`;
+
+const params = {
+  Records: [
+    {
+      Data: "Data",
+      PartitionKey: "PartitionKey"
+    }
+  ],
+  StreamName
+};
+
+const beforeAll = async () => {
+  const ShardCount = 1;
+  await v2Client.createStream({ StreamName, ShardCount }).promise();
+  await v2Client.waitFor("streamExists", { StreamName }).promise();
+
+  let response = await v2Client.listShards({ StreamName }).promise();
+  const { ShardId } = response.Shards[0];
+  const ShardIteratorType = "LATEST";
+  response = await v2Client
+    .getShardIterator({ ShardId, StreamName, ShardIteratorType })
+    .promise();
+  ShardIterator = response.ShardIterator;
+};
+
 const getV2Response = async clientParams => {
-  const client = new AWS.DynamoDB(clientParams);
-  return client.listTables().promise();
+  await v2Client.putRecords(params).promise();
+  return v2Client.getRecords({ ShardIterator }).promise();
 };
 
 const getV3Response = async clientParams => {
-  const client = new DynamoDB(clientParams);
-  return client.listTables({});
+  await v3Client.putRecords(params);
+  return v3Client.getRecords({ ShardIterator });
 };
 
 const getV2BrowserResponse = async () => {
@@ -39,7 +69,13 @@ const getV3BrowserResponse = async () =>
     })
   });
 
+const afterAll = async () => {
+  await v3Client.deleteStream({ StreamName });
+};
+
 module.exports = {
+  beforeAll,
+  afterAll,
   getV2Response,
   getV3Response,
   getV2BrowserResponse,
