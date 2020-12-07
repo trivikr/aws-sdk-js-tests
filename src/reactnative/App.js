3116a16a206d90df77500c5f49b381a0c3349735
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2020-12-07 15:04:32 -0800

    feat: replace expo with react-native CLI project

--- src/reactnative/App.js
+++ src/reactnative/App.js
@@ -1,61 +1,39 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
-import { getV2BrowserResponse, getV3BrowserResponse } from "../shared/utils";
+import {getV2BrowserResponse, getV3BrowserResponse} from '../shared/utils';
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu",
-});
+const App = () => {
+  const [v2Response, setV2Response] = useState('');
+  const [v3Response, setV3Response] = useState('');
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = { v2Response: "", v3Response: "" };
-  }
-
-  async componentDidMount() {
+  useEffect(async () => {
     const v2Response = await getV2BrowserResponse();
     const v3Response = await getV3BrowserResponse();
 
-    this.setState({
-      v2Response: JSON.stringify(v2Response, null, 2),
-      v3Response: JSON.stringify(v3Response, null, 2),
-    });
-  }
+    setV2Response(JSON.stringify(v2Response, null, 2));
+    setV3Response(JSON.stringify(v3Response, null, 2));
+  });
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.content}>
-          v2 response: {this.state.v2Response}!
-        </Text>
-        <Text style={styles.content}>
-          v3 response: {this.state.v3Response}!
-        </Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>v2 response: {this.state.v2Response}!</Text>
+      <Text style={styles.content}>v3 response: {this.state.v3Response}!</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   content: {
     fontSize: 20,
     margin: 10,
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-  },
 });
+
+export default App;
